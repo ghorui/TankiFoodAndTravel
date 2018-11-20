58c4c1b4be1ceb196701f384af0738ce4086b7e2
Tarun Kumar Ghorui <tarun.kumar.ghorui@accenture.com>
2018-11-20 19:25:53 +0530

    Initial check in

---
+++ TankiFoodAndTravel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TankiFoodAndTravel.Startup))]
+namespace TankiFoodAndTravel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
